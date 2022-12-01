0ebdc478345358264ffa04cc35c0df174fe1e1d5
Marat <Shagiakhmetov2499@gmail.com>
2022-12-01 16:47:34 +0300

    Initial Commit

---
+++ CountriesQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesQuiz
+//
+//  Created by Marat Shagiakhmetov on 01.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

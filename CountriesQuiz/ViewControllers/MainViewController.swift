6c8e93a9dc04570dc068bdeea09c8d65819dc645
Marat <Shagiakhmetov2499@gmail.com>
2022-12-05 22:54:36 +0300

    Added button for start game quiz of flags

--- CountriesQuiz/ViewControllers/MainViewController.swift
+++ CountriesQuiz/ViewControllers/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    
+    // MARK: - Private properties
     private lazy var labelMainCountries: UILabel = {
         let label = setLabel(
             title: "Countries",
@@ -63,13 +63,114 @@ class MainViewController: UIViewController {
         return image
     }()
     
+    private lazy var buttonQuizOfFlags: UIButton = {
+        let button = setButton(title: "Quiz of flags",
+                               size: 22,
+                               colorTitle: UIColor(
+                                red: 184/255,
+                                green: 247/255,
+                                blue: 252/255,
+                                alpha: 1
+                               ),
+                               colorBackgroud: UIColor(
+                                red: 125/255,
+                                green: 222/255,
+                                blue: 255/255,
+                                alpha: 0.15
+                               ),
+                               radiusCorner: 10,
+                               borderWidth: 3,
+                               borderColor: UIColor(
+                                red: 184/255,
+                                green: 247/255,
+                                blue: 252/255,
+                                alpha: 1).cgColor,
+                               shadowColor: UIColor(
+                                red: 54/255,
+                                green: 55/255,
+                                blue: 252/255,
+                                alpha: 1
+                               ).cgColor,
+                               radiusShadow: 3,
+                               shadowOffsetWidth: 2,
+                               shadowOffsetHeight: 2
+        )
+        button.addTarget(self, action: #selector(startQuizOfFlags), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var imageQuestionmark: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "questionmark.square.dashed")
+        image.tintColor = UIColor(
+            red: 184/255,
+            green: 247/255,
+            blue: 252/255,
+            alpha: 1
+        )
+        image.frame = CGRect(x: 180, y: 350, width: 75, height: 55)
+        image.transform = image.transform.rotated(by: .pi / -9)
+        image.layer.shadowColor = CGColor(
+            red: 54/255,
+            green: 55/255,
+            blue: 252/255,
+            alpha: 1
+        )
+        image.layer.shadowRadius = 2.5
+        image.layer.shadowOpacity = 1
+        image.layer.shadowOffset = CGSize(width: 0, height: 2)
+        return image
+    }()
+    
+    private lazy var imageFilemenu: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "filemenu.and.selection")
+        image.tintColor = UIColor(
+            red: 184/255,
+            green: 247/255,
+            blue: 252/255,
+            alpha: 1
+        )
+        image.frame = CGRect(x: 138, y: 350, width: 75, height: 55)
+        image.transform = image.transform.rotated(by: .pi / -9)
+        image.layer.shadowColor = CGColor(
+            red: 54/255,
+            green: 55/255,
+            blue: 252/255,
+            alpha: 1
+        )
+        image.layer.shadowRadius = 2.5
+        image.layer.shadowOpacity = 1
+        image.layer.shadowOffset = CGSize(width: 0, height: 2)
+        return image
+    }()
+    
+    private lazy var labelQuizOfFlags: UILabel = {
+        let label = setLabel(
+            title: "Quiz of flags",
+            size: 16,
+            style: "Apple SD Gothic Neo",
+            color: UIColor(
+                red: 184/255,
+                green: 227/255,
+                blue: 252/255,
+                alpha: 1
+            )
+        )
+        return label
+    }()
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
 //        setupMenu()
-        setupSubviews(subviews: imageMain, labelMainCountries, labelMainQuiz)
+        setupSubviews(subviews: imageMain,
+                      labelMainCountries,
+                      labelMainQuiz,
+                      buttonQuizOfFlags
+        )
         setConstraints()
     }
-    
+    // MARK: - Private methods
     private func setupMenu() {
         
     }
@@ -79,14 +180,14 @@ class MainViewController: UIViewController {
             view.addSubview(subview)
         }
     }
-    
+    // MARK: - Set constraints
     private func setConstraints() {
         imageMain.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageMain.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            imageMain.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            imageMain.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            imageMain.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+            imageMain.topAnchor.constraint(equalTo: view.topAnchor),
+            imageMain.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            imageMain.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageMain.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
         
         labelMainCountries.translatesAutoresizingMaskIntoConstraints = false
@@ -100,11 +201,22 @@ class MainViewController: UIViewController {
         NSLayoutConstraint.activate([
             labelMainQuiz.topAnchor.constraint(equalTo: labelMainCountries.topAnchor, constant: 45),
             labelMainQuiz.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 220),
-            labelMainQuiz.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50)
+            labelMainQuiz.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        buttonQuizOfFlags.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            buttonQuizOfFlags.topAnchor.constraint(equalTo: labelMainQuiz.topAnchor, constant: 150),
+            buttonQuizOfFlags.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            buttonQuizOfFlags.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
     }
+    
+    @objc private func startQuizOfFlags() {
+        print("start game!")
+    }
 }
-
+// MARK: - Setup label
 extension MainViewController {
     private func setLabel(title: String,
                           size: CGFloat,
@@ -130,5 +242,36 @@ extension MainViewController {
                                           height: shadowOffsetHeight ?? 0
         )
         return label
+    }
+}
+// MARK: - Setup button
+extension MainViewController {
+    private func setButton(title: String,
+                           style: String? = nil,
+                           size: CGFloat,
+                           colorTitle: UIColor? = nil,
+                           colorBackgroud: UIColor? = nil,
+                           radiusCorner: CGFloat,
+                           borderWidth: CGFloat? = nil,
+                           borderColor: CGColor? = nil,
+                           shadowColor: CGColor? = nil,
+                           radiusShadow: CGFloat? = nil,
+                           shadowOffsetWidth: CGFloat? = nil,
+                           shadowOffsetHeight: CGFloat? = nil) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(colorTitle, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: size, weight: .semibold)
+        button.backgroundColor = colorBackgroud
+        button.layer.cornerRadius = radiusCorner
+        button.layer.borderWidth = borderWidth ?? 0
+        button.layer.borderColor = borderColor
+        button.layer.shadowColor = shadowColor
+        button.layer.shadowOpacity = 1
+        button.layer.shadowRadius = radiusShadow ?? 0
+        button.layer.shadowOffset = CGSize(width: shadowOffsetWidth ?? 0,
+                                           height: shadowOffsetHeight ?? 0
+        )
+        return button
     }
 }

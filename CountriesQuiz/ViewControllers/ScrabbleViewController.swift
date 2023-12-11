5924edc2429500169e76f2ebda6d7d425f9cab8b
Marat <Shagiakhmetov2499@gmail.com>
2023-12-11 23:55:10 +0300

    Optimized code in IncorrectAnswersViewController and deleted extra code. Added new game type ScrabbleViewController.

---
+++ CountriesQuiz/ViewControllers/ScrabbleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScrabbleViewController.swift
+//  CountriesQuiz
+//
+//  Created by Marat Shagiakhmetov on 11.12.2023.
+//
+
+import UIKit
+
+class ScrabbleViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
